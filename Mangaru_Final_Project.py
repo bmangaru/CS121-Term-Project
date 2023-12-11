bf5793e20c3243730669272586f352a9c015d470
bmangaru <144833390+bmangaru@users.noreply.github.com>
2023-12-11 19:19:51 -0400

    brian

--- Mangaru_Final_Project.py
+++ Mangaru_Final_Project.py
@@ -4,15 +4,19 @@ from datetime import datetime
 
 # Gets the current time
 def get_current_time():
-    return datetime.now().time()
+    return datetime.now()
 
 # Determines if the current time is morning or afternoon
 def is_morning(current_time):
-    return current_time < datetime.strptime("12:00", "%H:%M").time()
+    return current_time.hour < 12
 
 # Formats the current time from 24-hour format to 12-hour format
 def format_time_12_hour(current_time):
     return current_time.strftime("%I:%M %p")
+
+# Formats the current date
+def format_date(current_time):
+    return current_time.strftime("%Y-%m-%d")
 
 # Creates a greeting message saying good morning or good afternoon during the appropriate times and the current time
 def get_greeting_message():
@@ -28,22 +32,20 @@ def get_greeting_message():
 
 # Asks whether the user has taken their medicine and logs the response and time in a file
 def log_medicine_taken():
-    while True:
-        response = input("Have you taken your medicine? (y/n): ").lower()
-        current_time = get_current_time()
-        formatted_time = format_time_12_hour(current_time)
+    response = input("Have you taken your medicine? (y/n): ").lower()
+    current_time = get_current_time()
+    formatted_date = format_date(current_time)
+    formatted_time = format_time_12_hour(current_time)
 
-        with open("medicine_log.txt", "a") as log_file:
-            if response == 'y':
-                log_file.write(f"Medicine taken at {formatted_time}\n")
-                print("Medicine logged successfully.")
-                break  # Exit the loop if a valid response is provided
-            elif response == 'n':
-                log_file.write(f"Medicine not taken at {formatted_time}\n")
-                print("Medicine not logged.")
-                break  # Exit the loop if a valid response is provided
-            else:
-                print("Invalid response. Please enter 'y' or 'n'.")
+    with open("medicine_log.csv", "a") as log_file:
+        if response == 'y':
+            log_file.write(f"{formatted_date},{formatted_time},1\n")
+            print("Medicine logged successfully.")
+        elif response == 'n':
+            log_file.write(f"{formatted_date},{formatted_time},0\n")
+            print("Medicine not logged.")
+        else:
+            print("Invalid response. Please enter 'y' or 'n'.")
 
 def main():
     # PART A GREETING
@@ -58,8 +60,9 @@ def main():
     # Check if it's midnight and medicine is not logged, then log as not taken
     current_time = get_current_time()
     if current_time.hour == 0 and current_time.minute == 0:
-        with open("medicine_log.txt", "a") as log_file:
-            log_file.write(f"Medicine not taken at midnight\n")
+        formatted_date = format_date(current_time)
+        with open("medicine_log.csv", "a") as log_file:
+            log_file.write(f"{formatted_date},{current_time.strftime('%I:%M %p')},0\n")
         print("Automatic log: Medicine not taken at midnight.")
 
 if __name__ == "__main__":
