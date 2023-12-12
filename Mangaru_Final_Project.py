7edacd43bba8bb693fac5271ce0269ec76d47345
bmangaru <144833390+bmangaru@users.noreply.github.com>
2023-12-12 12:04:29 -0400

    brian

--- Mangaru_Final_Project.py
+++ Mangaru_Final_Project.py
@@ -9,18 +9,24 @@ statuses = []
 
 # PART A GREETING
 
+#uses the datetime module to retrieve the current date and time.
 def get_current_time():
     return datetime.now()
 
+#checks to see if the current time is before noon, note the time here is in military time
+#EX: 11== morning, 13 aka 1PM == afternoon
 def is_morning(current_time):
     return current_time.hour < 12
 
+#formats the time to 12hour clock
 def format_time_12_hour(current_time):
     return current_time.strftime("%I:%M %p")
 
+#formats the date into year-month-day format
 def format_date(current_time):
     return current_time.strftime("%Y-%m-%d")
 
+#Using the result from is_morning function to print good moring during the appropiate times, else print good afternoon
 def get_greeting_message():
     current_time = get_current_time()
     formatted_time = format_time_12_hour(current_time)
@@ -32,12 +38,20 @@ def get_greeting_message():
 
 # PART B MEDICINE LOGGING
 
+#uses a while loop to prompt user if they have taken there medicine, y==yes, n==no, if user input is not 'y' or 'n' while loop will 
+# prompt user that there input was invalid and ask the user to try again; .lower() is used to convert Y to y or N to n
+
 def log_medicine_taken():
     current_time = get_current_time()
     current_date = format_date(current_time)
 
     while True:
         response = input("Have you taken your medicine? (y/n): ").lower()
+
+#if the user does choose to take there medicine, program will give the user instructions on how to use there medicine
+#then prints a message that there medicine was logged successfully
+# in csv file, current date, current time, and status will be appended to the file
+#Status=1: medicine was taken
 
         if response == 'y':
             status = 1
@@ -55,16 +69,25 @@ def log_medicine_taken():
             print("Medicine logged successfully.")
             break
 
+#if user does not chooese to take medcine
+#prints message that medicine was logged as not taken
+# in csv file, current date, current time, and status will be appended to the file
+#Status=0: medicine was NOT taken
+
         elif response == 'n':
             status = 0
             dates.append(current_date)
             times.append(current_time.strftime('%I:%M %p'))
             statuses.append(status)
-            print("Medicine not logged.")
+            print("Medicine logged as not taken.")
             break
+
+#ALL U HAD TO DO WAS TYPE Y OR N!!  TRY AGAIN!!
 
         else:
             print("Invalid response. Please enter 'y' or 'n'.")
+
+#Data is appeneded to csv file in data folder
 
     save_data_to_csv()
 
@@ -76,15 +99,22 @@ def save_data_to_csv():
 
 # PART C STREAK COUNTING
 
+#attempts to open csv file for reading 
+#if csv file is fould, reads all lines of the csv file
+
 def count_streak():
     csv_path = "Data/Magaru-Medicine-Log.csv"
     try:
         with open(csv_path, "r") as log_file:
             lines = log_file.readlines()
+
+ # only ran in the case where the log file doesn't exist, hopefully u will never run
     except FileNotFoundError:
-        # Handle the case where the log file doesn't exist
         lines = []
 
+#streak==0, 
+#starting at the last entery in the file(bottom of file), makes sure column has 3 parts (Date,Time,Status)
+#If there is a 1 in the status column, add 1 to streak variable until there is a zero in the status column 
     streak = 0
     for line in reversed(lines):
         parts = line.strip().split(',')
@@ -99,11 +129,16 @@ def count_streak():
 
 # PART D AVERAGE CALCULATION
 
+#attempts to open csv file for reading 
+#if csv file is fould, reads all lines of the csv file
+
 def calculate_average_status():
     csv_path = "Data/Magaru-Medicine-Log.csv"
     try:
         with open(csv_path, "r") as log_file:
             lines = log_file.readlines()
+            
+ # only ran in the case where the log file doesn't exist, hopefully u will never run
     except FileNotFoundError:
         return None  # Return None if the log file doesn't exist
 
@@ -163,7 +198,7 @@ def main():
             "You did it again! Another day, another win. Your persistence is bringing you closer to your health goals.",
             "One more day checked off! Your commitment to your well-being is truly commendable. Keep the momentum going!",
             "Today's achievement is a step forward in your health journey. You're making progress with each passing day!",
-            "Celebrate your victory today – you're making strides toward a healthier and happier you. Keep pushing forward!",
+            "Celebrate your victory today  you're making strides toward a healthier and happier you. Keep pushing forward!",
             "Kudos! One more day in the bag. Your consistency is paving the way for a brighter and healthier future."
         ]))
     elif current_streak > 1:
@@ -181,7 +216,8 @@ def main():
             "You're building a foundation for lasting change. Celebrate today's win in your health journey!"
         ]))
 
-    # PART D AVERAGE CALCULATION AND DISPLAY
+    # PART D AVERAGE CALCULATION AND DISPLAY as percent
+
     average_status = calculate_average_status()
     if average_status is not None:
         print(f"\nAverage of taking medicine: {average_status * 100:.2f}%")
