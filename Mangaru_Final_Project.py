31417a7c98d956f6f0f6d8ca3dc0e9bd8132ffc3
bmangaru <144833390+bmangaru@users.noreply.github.com>
2023-12-17 20:04:20 -0400

    brianmangaru

--- Mangaru_Final_Project.py
+++ Mangaru_Final_Project.py
@@ -2,179 +2,139 @@ from datetime import datetime
 import random
 import os
 
-# Lists to store medicine log data
-dates = []
-times = []
-statuses = []
 
-# PART A GREETING
+class MedicineLogger:
+    def __init__(self):
+        self.dates = []
+        self.times = []
+        self.statuses = []
 
-#uses the datetime module to retrieve the current date and time.
-def get_current_time():
-    return datetime.now()
+    def get_current_time(self):
+        return datetime.now()
 
-#checks to see if the current time is before noon, note the time here is in military time
-#EX: 11== morning, 13 aka 1PM == afternoon
-def is_morning(current_time):
-    return current_time.hour < 12
+    def is_morning(self, current_time):
+        return current_time.hour < 12
 
-#formats the time to 12hour clock
-def format_time_12_hour(current_time):
-    return current_time.strftime("%I:%M %p")
+    def format_time_12_hour(self, current_time):
+        return current_time.strftime("%I:%M %p")
 
-#formats the date into year-month-day format
-def format_date(current_time):
-    return current_time.strftime("%Y-%m-%d")
+    def format_date(self, current_time):
+        return current_time.strftime("%Y-%m-%d")
 
-#Using the result from is_morning function to print good moring during the appropiate times, else print good afternoon
-def get_greeting_message():
-    current_time = get_current_time()
-    formatted_time = format_time_12_hour(current_time)
+    def get_greeting_message(self):
+        current_time = self.get_current_time()
+        formatted_time = self.format_time_12_hour(current_time)
 
-    if is_morning(current_time):
-        return f"Good morning Brian! The current time is {formatted_time}."
-    else:
-        return f"Good afternoon Brian! The current time is {formatted_time}."
-
-# PART B MEDICINE LOGGING
-
-#uses a while loop to prompt user if they have taken there medicine, y==yes, n==no, if user input is not 'y' or 'n' while loop will 
-# prompt user that there input was invalid and ask the user to try again; .lower() is used to convert Y to y or N to n
-
-def log_medicine_taken():
-    current_time = get_current_time()
-    current_date = format_date(current_time)
-
-    while True:
-        response = input("Have you taken your medicine? (y/n): ").lower()
-
-#if the user does choose to take there medicine, program will give the user instructions on how to use there medicine
-#then prints a message that there medicine was logged successfully
-# in csv file, current date, current time, and status will be appended to the file
-#Status=1: medicine was taken
-
-        if response == 'y':
-            status = 1
-            print("\nUsing your asthma inhaler is easy:")
-            print("  - Shake inhaler well.")
-            print("  - Take off the cap and breathe out.")
-            print("  - Hold the inhaler with your finger on top and your thumb underneath.")
-            print("  - Put the mouthpiece in your mouth, close your lips, and breathe in slowly when you press the spray button.")
-            print("  - Hold your breath for 10 seconds, then breathe out.")
-          
-
-            dates.append(current_date)
-            times.append(current_time.strftime('%I:%M %p'))
-            statuses.append(status)
-            print("Medicine logged successfully.")
-            break
-
-#if user does not chooese to take medcine
-#prints message that medicine was logged as not taken
-# in csv file, current date, current time, and status will be appended to the file
-#Status=0: medicine was NOT taken
-
-        elif response == 'n':
-            status = 0
-            dates.append(current_date)
-            times.append(current_time.strftime('%I:%M %p'))
-            statuses.append(status)
-            print("Medicine logged as not taken.")
-            break
-
-#ALL U HAD TO DO WAS TYPE Y OR N!!  TRY AGAIN!!
-
+        if self.is_morning(current_time):
+            return f"Good morning Brian! The current time is {formatted_time}."
         else:
-            print("Invalid response. Please enter 'y' or 'n'.")
+            return f"Good afternoon Brian! The current time is {formatted_time}."
 
-#Data is appeneded to csv file in data folder
+    def log_medicine_taken(self):
+        current_time = self.get_current_time()
+        current_date = self.format_date(current_time)
 
-    save_data_to_csv()
+        while True:
+            response = input("Have you taken your medicine? (y/n): ").lower()
 
-def save_data_to_csv():
-    csv_path = "Data/Magaru-Medicine-Log.csv"
-    with open(csv_path, "a") as log_file:
-        for i in range(len(dates)):
-            log_file.write(f"{dates[i]},{times[i]},{statuses[i]}\n")
+            if response == 'y':
+                status = 1
+                print("\nUsing your asthma inhaler is easy:")
+                print("  - Shake inhaler well.")
+                print("  - Take off the cap and breathe out.")
+                print("  - Hold the inhaler with your finger on top and your thumb underneath.")
+                print("  - Put the mouthpiece in your mouth, close your lips, and breathe in slowly when you press the spray button.")
+                print("  - Hold your breath for 10 seconds, then breathe out.")
 
-# PART C STREAK COUNTING
+                self.dates.append(current_date)
+                self.times.append(current_time.strftime('%I:%M %p'))
+                self.statuses.append(status)
+                print("Medicine logged successfully.")
+                break
 
-#attempts to open csv file for reading 
-#if csv file is fould, reads all lines of the csv file
+            elif response == 'n':
+                status = 0
+                self.dates.append(current_date)
+                self.times.append(current_time.strftime('%I:%M %p'))
+                self.statuses.append(status)
+                print("Medicine logged as not taken.")
+                break
 
-def count_streak():
-    csv_path = "Data/Magaru-Medicine-Log.csv"
-    try:
-        with open(csv_path, "r") as log_file:
-            lines = log_file.readlines()
+            else:
+                print("Invalid response. Please enter 'y' or 'n'.")
 
- # only ran in the case where the log file doesn't exist, hopefully u will never run
-    except FileNotFoundError:
-        lines = []
+        self.save_data_to_csv()
 
-#streak==0, 
-#starting at the last entery in the file(bottom of file), makes sure column has 3 parts (Date,Time,Status)
-#If there is a 1 in the status column, add 1 to streak variable until there is a zero in the status column 
-    streak = 0
-    for line in reversed(lines):
-        parts = line.strip().split(',')
-        # Check if the line has exactly three parts and the status is '1' (medicine taken)
-        if len(parts) == 3 and parts[2] == '1':
-            streak += 1
-        else:
-            # Break the loop if the streak is broken (medicine wasn't taken on a particular day)
-            break
+    def save_data_to_csv(self):
+        csv_path = "Data/Magaru-Medicine-Log.csv"
+        with open(csv_path, "a") as log_file:
+            for i in range(len(self.dates)):
+                log_file.write(f"{self.dates[i]},{self.times[i]},{self.statuses[i]}\n")
 
-    return streak
+    def count_streak(self):
+        csv_path = "Data/Magaru-Medicine-Log.csv"
+        try:
+            with open(csv_path, "r") as log_file:
+                lines = log_file.readlines()
 
-# PART D AVERAGE CALCULATION
+        except FileNotFoundError:
+            lines = []
 
-#attempts to open csv file for reading 
-#if csv file is fould, reads all lines of the csv file
+        streak = 0
+        for line in reversed(lines):
+            parts = line.strip().split(',')
+            if len(parts) == 3 and parts[2] == '1':
+                streak += 1
+            else:
+                break
 
-def calculate_average_status():
-    csv_path = "Data/Magaru-Medicine-Log.csv"
-    try:
-        with open(csv_path, "r") as log_file:
-            lines = log_file.readlines()
-            
- # only ran in the case where the log file doesn't exist, hopefully u will never run
-    except FileNotFoundError:
-        return None  # Return None if the log file doesn't exist
+        return streak
 
-    # Skip the first line (headers) and extract status values
-    statuses_from_file = [int(line.strip().split(',')[2]) for line in lines[1:] if len(line.strip().split(',')) >= 3]
+    def calculate_average_status(self):
+        csv_path = "Data/Magaru-Medicine-Log.csv"
+        try:
+            with open(csv_path, "r") as log_file:
+                lines = log_file.readlines()
 
-    if not statuses_from_file:
-        return None  # Return None if there are no valid statuses in the file
+        except FileNotFoundError:
+            return None
 
-    total_status = sum(statuses_from_file)
-    total_entries = len(statuses_from_file)
-    average_status = total_status / total_entries
-    return average_status
+        statuses_from_file = [int(line.strip().split(',')[2]) for line in lines[1:] if
+                              len(line.strip().split(',')) >= 3]
+
+        if not statuses_from_file:
+            return None
+
+        total_status = sum(statuses_from_file)
+        total_entries = len(statuses_from_file)
+        average_status = total_status / total_entries
+        return average_status
+
 
 def main():
+    logger = MedicineLogger()
+
     # PART A GREETING
-    greeting_message = get_greeting_message()
+    greeting_message = logger.get_greeting_message()
     print(greeting_message)
 
     # PART B MEDICINE LOGGING
-    log_medicine_taken()
+    logger.log_medicine_taken()
 
     # Check if it's midnight and medicine is not logged, then log as not taken
-    current_time = get_current_time()
+    current_time = logger.get_current_time()
     if current_time.hour == 0 and current_time.minute == 0:
-        formatted_date = format_date(current_time)
-        dates.append(formatted_date)
-        times.append(current_time.strftime('%I:%M %p'))
-        statuses.append(0)
+        formatted_date = logger.format_date(current_time)
+        logger.dates.append(formatted_date)
+        logger.times.append(current_time.strftime('%I:%M %p'))
+        logger.statuses.append(0)
         print("Automatic log: Medicine not taken at midnight.")
 
         # Save data to CSV file
-        save_data_to_csv()
+        logger.save_data_to_csv()
 
     # PART C STREAK COUNTING
-    current_streak = count_streak()
+    current_streak = logger.count_streak()
     print(f"\nCurrent Streak: {current_streak} days")
 
     # Print a motivational message based on the streak
@@ -217,8 +177,7 @@ def main():
         ]))
 
     # PART D AVERAGE CALCULATION AND DISPLAY as percent
-
-    average_status = calculate_average_status()
+    average_status = logger.calculate_average_status()
     if average_status is not None:
         print(f"\nAverage of taking medicine: {average_status * 100:.2f}%")
 
--- README.md
+++ README.md
@@ -8,21 +8,22 @@ CS 121
 
 12 December 2023
 
+Overview: 
 
-Overview:
+The program is a medicine logging system designed to assist an individual in tracking their medication usage, specifically an asthma inhaler. The system prompts the user to log whether they has taken their medicine, provides instructions if they have, and maintains a log of dates, times, and the status of medicine intake (1 for taken, 0 for not taken) in a CSV file. The program also calculates and displays a streak of consecutive days with medicine intake and calculates the average percentage of medicine taken over time. The main motivation is to encourage the user to consistently take there medication and provide motivational messages based on there streak.
 
-I made the program beacuse often times I forget to take my medicine so it is nice to have something that will keep track of my medication. It will allow me to also look at the logs to see how I am doing
+Features / Complexity:
 
-Features/Complexity:
+    Medicine Logging: The program prompts the user to log there medicine intake and provides instructions for using the inhaler.
 
-Greeting: Will print the current time, say good morning or good afternoon during the apporpiate times and prompt the user if they have taken there medicine
+    Data Storage: Logs are stored in a CSV file for historical tracking.
 
-Log File:
+    Streak Counting: Calculates and displays the current streak of consecutive days with medicine intake.
 
-Streak:
+    Average Calculation: Calculates and displays the average percentage of medicine taken over time.
 
-Percentage 
+    Motivational Messages: Provides motivational messages based on the streak, encouraging consistency.
 
-Favorite Part:
+    Favorite Part: My favorite part is the streak counter, which adds personality to the program.
 
-Least Favorite Part:
+    Least Favorite Part: My least favorite part is saving the information to a CSV file, something I struggled with in the labs and found to be the most time-consuming aspect.
